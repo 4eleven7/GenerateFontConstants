6ceb5b27d28583b5c392ac28ef31aa0c126c66e5
Dan Love <dan@daniellove.net>
2015-12-16 13:56:20 +0000

    Demo

---
+++ GenerateFontConstantsDemo/GenerateFontConstantsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenerateFontConstantsDemo
+//
+//  Created by Daniel Love on 16/12/2015.
+//  Copyright © 2015 Daniel Love. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
